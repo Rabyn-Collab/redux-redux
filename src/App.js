613f228540233b932c6779713989a9f916655237
Rabyn-Collab <babynshrestha64@gmail.com>
2023-10-03 08:56:14 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import RootLayOut from './components/RootLayOut'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
 import HomePage from './pages/HomePage'
-import Ramdom from './pages/Ramdom'
 
 
 const App = () => {
@@ -13,7 +12,7 @@ const App = () => {
       <Route path='/' element={<RootLayOut />} >
         <Route index element={<HomePage />} />
         <Route path='about' element={<About />} />
-        <Route path='random' element={<Ramdom />} />
+
         <Route path='*' element={<NotFound />} />
 
       </Route>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,31 +1,8 @@
-
-import useApi from '../hooks/useApi';
+import React from 'react'
 
 const HomePage = () => {
-
-  const [data, err, load] = useApi('https://www.themealdb.com/api/json/v1/1/categories.php');
-
-
-  if (load) {
-    return <h1>Loading....</h1>
-  }
-
-  if (err) {
-    return `${err}`
-  }
-
-
   return (
     <div>
-
-
-      {data && data.categories.map((cata, i) => {
-        return <div key={i}>
-          <h1>{cata.strCategory}</h1>
-          <img src={cata.strCategoryThumb} alt="" />
-          <p>{cata.strCategoryDescription}</p>
-        </div>
-      })}
 
     </div>
   )
