8363c869e355dca210538ca24f3a8008ad193837
Rabyn-Collab <babynshrestha64@gmail.com>
2023-10-06 08:31:05 +0545

    chages

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Display from './dash/Display'
 import Tech from './dash/Tech'
+import Info from './dash/Info'
+import Footer from './dash/Footer'
 
 const HomePage = () => {
   return (
     <div>
       <Display />
       <Tech />
+      <Info />
+      <Footer />
 
     </div>
   )
---
+++ src/pages/dash/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='relative '>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="black" fillOpacity="1" d="M0,192L1440,32L1440,320L0,320Z"></path>
+      </svg>
+      <div className='absolute bottom-[25%] text-white left-[45%]  '>
+        <h1>Develop bY AnonyMous</h1>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Footer
