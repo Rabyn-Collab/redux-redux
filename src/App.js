8d36e8c12db7790f10e0655c4489e4fa404f4ced
Rabyn-Collab <babynshrestha64@gmail.com>
2023-10-10 09:08:34 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import { Route, Routes } from 'react-router'
 import RootLayOut from './components/RootLayOut'
 import NotFound from './pages/NotFound'
 import HomePage from './pages/HomePage'
-import * as Gi from './pages/smaple';
 
 const App = () => {
 
 
 
-  console.log(Gi.data);
+
   return (
     <Routes>
       <Route path='/' element={<RootLayOut />} >
--- src/components/Crud.js
+++ src/components/Crud.js
@@ -10,7 +10,7 @@ import {
   Textarea
 } from "@material-tailwind/react";
 import { useFormik } from 'formik';
-
+import * as Yup from 'yup';
 
 const radioData = [
   { label: 'Male', value: 'male' },
@@ -26,7 +26,31 @@ const checkData = [
 
 
 
+
 const Crud = () => {
+
+  const crudSchema = Yup.object().shape({
+    username: Yup.string().min(4).max(25).required(),
+    email: Yup.string().matches(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'please provide valid email').required(),
+    gender: Yup.string().required(),
+    habits: Yup.array().min(1).required(),
+    country: Yup.string().required(),
+    msg: Yup.string().min(40).max(500).required(),
+
+
+    imageFile: Yup.mixed().test('fileType', 'Invalid file type', (value) => {
+      return value && ['image/jpeg', 'image/png', 'image/jpg'].includes(value.type);
+    }).test('fileSize', 'File too large', (value) =>
+      value && value.size <= 10 * 1024 * 1024
+    )
+
+
+
+
+
+
+  });
+
 
   const formik = useFormik({
     initialValues: {
@@ -40,24 +64,33 @@ const Crud = () => {
       imageReview: ''
     },
     onSubmit: (val) => {
-      console.log(val);
-    }
+      console.log(val.imageFile);
+    },
+    validationSchema: crudSchema
   });
 
   return (
     <Card color="transparent" shadow={false} className='  max-w-lg mx-auto  '>
       <form onSubmit={formik.handleSubmit} className=" mb-2 mt-10 ">
         <div className="mb-4 flex flex-col gap-6">
-          <Input
-            name='username'
-            value={formik.values.username}
-            onChange={formik.handleChange}
-            size="lg" label="Name" />
-          <Input
-            name='email'
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            size="lg" label="Email" />
+
+          <div>
+            <Input
+              name='username'
+              value={formik.values.username}
+              onChange={formik.handleChange}
+              size="lg" label="Name" />
+
+            {formik.errors.username && formik.touched.username && <h1 className='text-pink-800'>{formik.errors.username}</h1>}
+          </div>
+          <div>
+            <Input
+              name='email'
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              size="lg" label="Email" />
+            {formik.errors.email && formik.touched.email && <h1 className='text-pink-800'>{formik.errors.email}</h1>}
+          </div>
 
 
           <div className='space-y-2'>
@@ -69,7 +102,9 @@ const Crud = () => {
                   key={i} name="gender" label={radio.label} value={radio.value} />;
               })}
 
+
             </div>
+            {formik.errors.gender && formik.touched.gender && <h1 className='text-pink-800'>{formik.errors.gender}</h1>}
           </div>
 
 
@@ -85,6 +120,7 @@ const Crud = () => {
               })}
 
             </div>
+            {formik.errors.habits && formik.touched.habits && <h1 className='text-pink-800'>{formik.errors.habits}</h1>}
           </div>
 
 
@@ -97,23 +133,26 @@ const Crud = () => {
               <Option value='India'>India</Option>
               <Option value='China'>China</Option>
             </Select>
+            {formik.errors.country && formik.touched.country && <h1 className='text-pink-800'>{formik.errors.country}</h1>}
           </div>
 
           <div>
             <Textarea
-              // value={formik.values.msg}
-              onChange={(e) => {
-                console.log(e.target.value);
-              }}
+              value={formik.values.msg}
+              onChange={formik.handleChange}
+              name='msg'
               label="Message" />
+            {formik.errors.msg && formik.touched.msg && <h1 className='text-pink-800'>{formik.errors.msg}</h1>}
           </div>
 
           <div className='space-y-3'>
             <p>Select Your Image</p>
             <Input
+              name='imageFile'
               onChange={(e) => {
                 const file = e.target.files[0];
-                formik.setFieldValue('imageFile, file');
+
+                formik.setFieldValue('imageFile', file);
                 // const url = URL.createObjectURL(file);
                 // console.log(url);
 
@@ -121,13 +160,13 @@ const Crud = () => {
 
                 reader.readAsDataURL(file);
                 reader.addEventListener('load', (e) => {
-
                   formik.setFieldValue('imageReview', e.target.result);
                 })
 
 
               }}
               label="Image Select" type='file' accept="image/*" />
+            {formik.errors.imageFile && formik.touched.imageFile && <h1 className='text-pink-800'>{formik.errors.imageFile}</h1>}
             {formik.values.imageReview && <img src={formik.values.imageReview} alt="" />}
           </div>
 
