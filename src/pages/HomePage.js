9ecc2529d487bed1776d2150b5a535b56ebe40a5
Rabyn-Collab <babynshrestha64@gmail.com>
2023-10-13 08:18:03 +0545

    changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,25 @@
 import React from 'react'
-import Crud from '../components/Crud'
 import { useSelector } from 'react-redux'
-
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+} from "@material-tailwind/react";
 const HomePage = () => {
+  const d = [
+    { id: 1, title: 'hello' },
+    { id: 2, title: 'hello jee' },
+  ];
+  const m = d.filter((p) => p.id !== 1);
+  console.log(m);
 
+
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => setOpen(!open);
   const { todos } = useSelector((store) => store.todo);
 
 
@@ -17,10 +33,32 @@ const HomePage = () => {
           <img src={todo.imageReview} alt="" />
           <div className='flex justify-end space-x-4'>
             <button><i className="fa-solid fa-pen-to-square fa-lg"></i></button>
-            <button><i className="fa-solid fa-trash fa-lg"></i></button>
+            <button onClick={handleOpen}><i className="fa-solid fa-trash fa-lg"></i></button>
           </div>
         </div>
       })}
+
+
+
+      <Dialog open={open} handler={handleOpen}>
+        <DialogHeader>Are you sure ?</DialogHeader>
+        <DialogBody divider>
+          You want to remove this
+        </DialogBody>
+        <DialogFooter>
+          <Button
+            variant="text"
+            color="red"
+            onClick={handleOpen}
+            className="mr-1"
+          >
+            <span>Cancel</span>
+          </Button>
+          <Button variant="gradient" color="green" onClick={handleOpen}>
+            <span>Confirm</span>
+          </Button>
+        </DialogFooter>
+      </Dialog>
 
     </div>
   )
